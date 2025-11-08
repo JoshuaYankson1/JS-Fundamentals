7d32e99e975a194af8d11cf36890f7c9dc4d228e
JoshuaYankson1 <joshuayamoah2016@gmail.com>
2025-11-08 23:41:16 +0000

    using loop to print 3 seperate text same as 1-multi_languages.js

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 let myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-for (let loopvar = 0; loopvar < myArray.length; loopvar++){
-    console.log(myArray[loopvar]);
+for (let loop = 0; loop < myArray.length; loop++){
+    console.log(myArray[loop]);
 };
